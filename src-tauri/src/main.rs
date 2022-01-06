877f870cf5e68c4e24b188aee34a058535bd5502
sinyo-matu <sinyo_matu@icloud.com>
2022-01-06 17:26:39 +0900

    update add edit menu

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,8 +5,28 @@
 
 use std::ops::Deref;
 
+use tauri::{Menu, MenuItem, Submenu};
+
 fn main() {
+  let edit_menu = Submenu::new(
+    "編集",
+    Menu::new()
+      .add_native_item(MenuItem::Copy)
+      .add_native_item(MenuItem::Cut)
+      .add_native_item(MenuItem::Paste)
+      .add_native_item(MenuItem::Undo)
+      .add_native_item(MenuItem::Redo),
+  );
+  let app_menu = Submenu::new(
+    "SMT".to_string(),
+    Menu::new()
+      .add_native_item(MenuItem::About("PM".to_string()))
+      .add_native_item(MenuItem::Separator)
+      .add_native_item(MenuItem::Quit),
+  );
+  let menu = Menu::new().add_submenu(app_menu).add_submenu(edit_menu);
   tauri::Builder::default()
+    .menu(menu)
     .setup(|app| {
       let mut path_base = tauri::api::path::app_dir(app.config().deref()).unwrap();
       if tauri::api::dir::is_dir(&path_base).is_err() {
